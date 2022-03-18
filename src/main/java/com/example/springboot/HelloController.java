fcb97b06f357c7d703f2e1d52ffd6913bbcc7c7f
Vasanth Desikan <87298383+vdesikanvmware@users.noreply.github.com>
2022-03-18 14:43:32 +0530

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,11 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-<<<<<<< HEAD
-		return "Greetings from Spring Boot + Tanzu + 1647590401!";
-=======
-		return "Greetings from Spring Boot + Tanzu + 1647590401!";
->>>>>>> 1647590401f1647590401c1647590401a1647590401c1647590401eda1647590401d1647590401ad1647590401dde1647590401d1647590401cdb
+		return "Greetings from Spring Boot + Tanzu + 1647525601!";
 	}
 
 }
